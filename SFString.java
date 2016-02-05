a702e9e433f78224a63c7969eca144c6a41b8b3f
Sil3ntPr0mpt <gh0stsync.rt@gmail.com>
2016-02-05 15:46:36 +0700

    Create SFString.java

---
+++ SFString.java
@@ -0,0 +1,35 @@
+//Coded by Anuwat Khongchuai A.K.A Sil3ntPr0mpt at TRN-SEC Group.
+//RMUTSV String Test.
+//This is first part of full code.
+import java.util.Scanner; 
+public class SFString {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String base_alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		String input_alpha;
+		int i,j;
+		//int k,l;
+		//int count=0;
+		
+		System.out.println("Enter Alphabet > ");
+		
+		input_alpha = sc.nextLine();
+		
+		for(i=0;i<=base_alpha.length()-1;i++){
+			for(j=0;j<=input_alpha.length()-1;j++){
+				if(base_alpha.charAt(i) == input_alpha.charAt(j)){
+					System.out.print(base_alpha.charAt(i));
+					
+				}
+				
+			}
+				
+		}
+		System.out.println("\n");
+		
+		
+	}
+	
+}
